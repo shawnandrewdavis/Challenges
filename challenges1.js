36e12de1c8043b00eedd62c954aca307385e4be6
Shawn Davis <shawnandrewdavis@gmail.com>
2019-01-09 16:56:12 -0500

    first challenges

---
+++ challenges1.js
@@ -0,0 +1,6 @@
+//***********************
+// Challenge Assessment 1
+// ***********************
+
+// What is a palindrome?  A palindrome is a word that is spelled the same way, both forwards and backwards (i.e. bob, level, tacocat, racecar, etc.).  Notice that each of these words have the same spelling if you reversed the word.
+// Using only JavaScript, create a prompt telling the user, "Enter a word to see if it is a palindrome", then create a way to check if the word is in fact a palindrome.  If it is, have JavaScript console.log that their word is a palindrome; if it is not, have JavaScript console.log the opposite.
